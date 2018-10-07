403ddeb0f0a1469f6037c73a4f6c6f24c9a80550
Kevin Gurol <cagataygurol@gmail.com>
2018-10-07 15:33:16 +0000

    Dropdown menu is updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,6 @@
-$(document).ready(function(e){
-    $('.search-panel .dropdown-menu').find('a').click(function(e) {
-		e.preventDefault();
-		var param = $(this).attr("href").replace("#","");
-		var concept = $(this).html();
-		$('.search-panel span#search_concept').html(concept);
-		$('.input-group #search_param').val(param);
-		
-	});
+
+function city_change() {
 	
-});
+	document.getElementById("chosen-item").selectIndex = document.getElementById("city-select").selectIndex
+}
+
